86af90b725e1cd3f089bd1c30ee58fe6902291df
alunduil <alunduil@440015c7-4129-0410-830b-8fcee99ad2f7>
2009-09-07 04:51:22 +0000

    Adding the initial codeset of the compiler to the repository.

---
+++ compiler/trunk/include/symboltable.h
@@ -0,0 +1,223 @@
+/*
+    Definition of the Symbol Table defined for CSIS 435.
+    Copyright (C) 2009 Alex Brandt
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifndef SYMBOLTABLE_H
+#define SYMBOLTABLE_H
+
+#include <string>
+#include <map>
+#include <list>
+#include <ostream>
+#include <symboltableentry.h>
+
+namespace Environment
+{
+    class SymbolTable
+    {
+        public:
+            /**
+             * @brief Constructor
+             *
+             * Construct Symbol Table.
+             */
+            SymbolTable();
+
+            /**
+             * @brief Destructor
+             *
+             * Destructor Symbol Table.
+             */
+            ~SymbolTable();
+
+            /**
+             * @brief Find an entry in the table based on the lexeme.
+             * @param lexeme The unique identifier of the word representing the token.
+             * @param level An inout parameter that returns the number of references to
+             *   the table containing the desired lexeme.
+             * @return SymbolTableEntry pointer of the found entry with the desired
+             *   lexeme or null if not found.
+             *
+             * Search for the lexeme in the table and return a reference to the entry if
+             * it is found.  If it is not in the table return null so we can have testing
+             * on the return value for the user.
+             *
+             * @note Adding const after this prototype throws a compile error . . . why?
+             */
+            SymbolTableEntry * Find(const std::string lexeme, int &level);
+
+            /**
+             * @brief Overloaded version of the previous that takes an entry.
+             * @param entry The Symbol Table Entry reference to find.
+             * @param level The level of the entry in our sequence of tables.
+             * @return SymbolTableEntry pointer of the found entry with the desired
+             *   lexeme or null if not found.
+             *
+             * Same as the previous definition of Find, but takes a Symbol Table Entry
+             * rather than a lexeme.
+             */
+            SymbolTableEntry * Find(SymbolTableEntry &entry, int &level);
+
+            /**
+             * @brief Insert the lexeme into the table.
+             * @param lexeme The lexeme to be inserted.
+             * @param token The value of the token (found in TOKEN_VALUE)
+             * @pre lexeme does not exist in the table already.
+             *
+             * Check that the symbol is not already in the table and then insert
+             * it into the table if this is the case.  If the entry exists it will
+             * be overwritten.  CHECK THAT THE ENTRY DOES NOT EXIST BEFORE CALLING!
+             */
+            void Insert(const std::string lexeme, TOKEN_VALUE token = ID);
+
+            /**
+             * @brief Insert the Symbol Table Entry into the table.
+             * @param entry SymbolTableEntry reference to insert into the table.
+             * @pre entry does not exist in the table already.
+             *
+             * Check that the entry is not already in the table and then insert it.
+             * If the entry is already in the table it will be overwritten.
+             * CHECK THAT THE ENTRY DOES NOT EXIST BEFORE CALLING!
+             */
+            void Insert(SymbolTableEntry &entry);
+
+            /**
+             * @brief Push a new scope onto the System Table.
+             *
+             * Add another scope to the symbol table by putting it on an internal stack.
+             */
+            void Push();
+
+            /**
+             * @brief Pop the top most scope out of the Symbol Table.
+             *
+             * Remove the top level scoping from the Symbol Table.
+             */
+            void Pop();
+
+            /**
+             * @brief Stream insertion operator.
+             * @param out The output stream to send to.
+             * @param table The table to output.
+             * @return Output stream reference to stack stream insertions.
+             *
+             * Insert the table into the output stream.
+             */
+            friend std::ostream & operator<<(std::ostream &out, SymbolTable &table)
+            {
+                return table.print(out);
+            }
+
+        protected:
+            /**
+             * @brief Print the table with proper level indication.
+             * @param out The output stream to send to.
+             * @return Output the stream reference to stack stream insertions.
+             *
+             * Print the table with an appropriate level mark.
+             */
+            std::ostream & print(std::ostream &out);
+
+            class InternalSymbolTable
+            {
+                public:
+                    /**
+                     * @brief Destructor.
+                     *
+                     * Destructor.
+                     */
+                    ~InternalSymbolTable();
+
+                    /**
+                    * @brief Find an entry in the table based on the lexeme.
+                    * @param lexeme The unique identifier of the word representing the token.
+                    * @return SymbolTableEntry pointer of the found entry with the desired
+                    *   lexeme or null if not found.
+                    *
+                    * Search for the lexeme in the table and return a reference to the entry if
+                    * it is found.  If it is not in the table return null so we can have testing
+                    * on the return value for the user.
+                    */
+                    SymbolTableEntry * Find(const std::string lexeme);
+
+                    /**
+                    * @brief Overloaded version of the previous that takes an entry.
+                    * @param entry The Symbol Table Entry reference to find.
+                    * @return SymbolTableEntry pointer of the found entry with the desired
+                    *   lexeme or null if not found.
+                    *
+                    * Same as the previous definition of Find, but takes a Symbol Table Entry
+                    * rather than a lexeme.
+                    */
+                    SymbolTableEntry * Find(const SymbolTableEntry &entry);
+
+                    /**
+                    * @brief Insert the lexeme into the table.
+                    * @param lexeme The lexeme to be inserted.
+                    * @param token The value of the token (found in TOKEN_VALUE)
+                    * @pre lexeme does not exist in the table already.
+                    *
+                    * Check that the symbol is not already in the table and then insert
+                    * it into the table if this is the case.  If the entry exists it will
+                    * be overwritten.  CHECK THAT THE ENTRY DOES NOT EXIST BEFORE CALLING!
+                    */
+                    void Insert(const std::string lexeme, TOKEN_VALUE token = ID);
+
+                    /**
+                    * @brief Insert the Symbol Table Entry into the table.
+                    * @param entry SymbolTableEntry reference to insert into the table.
+                    * @pre entry does not exist in the table already.
+                    *
+                    * Check that the entry is not already in the table and then insert it.
+                    * If the entry is already in the table it will be overwritten.
+                    * CHECK THAT THE ENTRY DOES NOT EXIST BEFORE CALLING!
+                    */
+                    void Insert(SymbolTableEntry &entry);
+
+                    /**
+                     * @brief Stream insertion operator.
+                     * @param out The output stream to send to.
+                     * @param table The table to output.
+                     * @return Output stream reference to stack stream insertions.
+                     *
+                     * Insert the table into the output stream.
+                     */
+                    friend std::ostream & operator<<(std::ostream &out, InternalSymbolTable &table)
+                    {
+                        return table.print(out);
+                    }
+
+                protected:
+                    /**
+                     * @brief Print the table with proper level indication.
+                     * @param out The output stream to send to.
+                     * @return Output the stream reference to stack stream insertions.
+                     *
+                     * Insert the table with an appropriate level mark.
+                     */
+                    std::ostream & print(std::ostream &out);
+
+                private:
+                    std::map<std::string, SymbolTableEntry *> table;
+            };
+        private:
+            std::list<InternalSymbolTable *> tables;
+    };
+};
+
+#endif // SYMBOLTABLE_H
---
+++ compiler/trunk/include/symboltableentry.h
@@ -0,0 +1,185 @@
+/*
+    Definition of the Symbol Table Entry defined for CSIS 435.
+    Copyright (C) 2009 Alex Brandt
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
+#ifndef SYMBOLTABLEENTRY_H
+#define SYMBOLTABLEENTRY_H
+
+#include <string>
+#include <map>
+
+namespace Environment
+{
+    /**
+     * @brief A definition of the different tokens.
+     * @todo Define more of these values.
+     */
+    enum TOKEN_VALUE
+    {
+        ID = 256
+    };
+
+    /**
+     * @brief A definition of the different types.
+     * @todo Define these values.
+     */
+    enum TOKEN_TYPE
+    {
+        REAL = 513,
+        INTEGER,
+        BOOLEAN,
+        CHARACTER,
+        /**
+         * @todo Add: Enumerated Types, Subrange
+         */
+        RECORD,
+        ARRAY,
+        SET,
+        FILE,
+        POINTER
+    };
+
+    /**
+     * @brief An entry in a symbol table.
+     *
+     * This class requires 4 elements (lexeme, type, offset, token value), but
+     * allows itself to have other properties added to it by using the indexing
+     * operator.
+     *
+     * @note Add the others functionality later if time.
+     */
+    class SymbolTableEntry
+    {
+        public:
+            /**
+             * @brief Set the lexeme for this entry.
+             * @param lexeme The name of the entry in the table.
+             *
+             * Change the name or lexeme of this entry in the symbol table.
+             */
+            void SetLexeme(const std::string lexeme);
+
+            /**
+             * @brief Get the lexeme for this entry.
+             * @return std::string containing entry's identifier.
+             *
+             * Get the name of the entry in the table.
+             */
+            std::string GetLexeme() const;
+
+            /**
+             * @brief Set the token's type.
+             * @param value The type of token stored.
+             *
+             * Change the type of token of this entry in the symbol table.
+             */
+            void SetTokenValue(const TOKEN_VALUE value);
+
+            /**
+             * @brief Get the token's type.
+             * @return TOKEN_VALUE specifying the type of the entry.
+             *
+             * Get the type of token of the entry.
+             */
+            TOKEN_VALUE GetTokenValue() const;
+
+            /**
+             * @brief Set the ARI addressing offset.
+             * @param offset The offset (in bytes) of the entry.
+             *
+             * Change the offset of the value's position in the ARI.
+             */
+            void SetOffSet(const int offset);
+
+            /**
+             * @brief Get the ARI addressing offset.
+             * @return int specifying the address offset of the value in the ARI.
+             *
+             * Get the offset of the value's position in the ARI.
+             * Result is measured in bytes.
+             */
+            int GetOffSet() const;
+
+            /**
+             * @brief Set the type of the entry.
+             * @param type The type of the entry.
+             *
+             * Set the type of the entry to the type passed.
+             */
+            void SetType(const TOKEN_TYPE type);
+
+            /**
+             * @brief Get the type of the entry.
+             * @return The type of the entry.
+             *
+             * Gets the type of the entry.
+             */
+            TOKEN_TYPE GetType() const;
+
+            /**
+             * @brief Constructor
+             * @param lexeme The identifier for the token.
+             * @param offset The ARI addressing offset.
+             * @param value The value of the token.
+             *
+             * Construct Symbol Table Entry.
+             */
+            SymbolTableEntry(const std::string lexeme, const int offset = 0, const TOKEN_VALUE value = ID);
+
+            /**
+             * @brief Equivalency Operator
+             * @param that The other entry to compare against.
+             * @return bool indicating similarity between the two entries.
+             *
+             * Checks if the two entries contain the same lexeme.  The lexeme is our unique
+             * identifier and our primary key in this ad-hoc database.
+             */
+            bool operator==(const SymbolTableEntry &that) const;
+
+            /**
+             * @brief Stream insertion operator.
+             * @param out The output stream to send to.
+             * @param entry The entry to output.
+             * @return Output stream reference to stack stream insertions.
+             *
+             * Insert the entry into the output stream.
+             */
+            friend std::ostream & operator<<(std::ostream &out, const SymbolTableEntry &entry)
+            {
+                return entry.print(out);
+            }
+
+        protected:
+            /**
+             * @brief Print out the entry to the specified stream.
+             * @param out The stream to print to.
+             * @return A reference to the original stream.
+             *
+             * Insert the entry into the output stream.
+             */
+            std::ostream & print(std::ostream &out) const;
+
+        private:
+            std::string lexeme;
+            int offset;
+            TOKEN_VALUE value;
+            TOKEN_TYPE type;
+    };
+};
+
+#endif // SYMBOLTABLEENTRY_H
